8cb9d1abc46676f130de30a7bd8d0b6ffa0148c1
Paula <paula.troszt@stud.tu-darmstadt.de>
2025-12-08 19:10:27 +0100

    added undo button + layout update

--- frontend/src/pages/certificate/CounterexampleDisplay.tsx
+++ frontend/src/pages/certificate/CounterexampleDisplay.tsx
@@ -1,6 +1,8 @@
 import '../App.css';
 import { useTranslation } from 'react-i18next';
-import { Box, Grid, Button } from '@mui/material';
+import { Box, Grid, Button, IconButton } from '@mui/material';
+import UndoIcon from '@mui/icons-material/Undo';
+import SyncAltIcon from '@mui/icons-material/SyncAlt';
 import { useEffect, useLayoutEffect, useRef, useState } from 'react';
 import { ParseUtils } from '../../utils/parseUtils.ts';
 import { useButtonUtils } from '../../utils/buttonUtils.ts';
@@ -48,6 +50,9 @@ function CounterexampleDisplay() {
   const initialNode = certificate.graph.nodes.filter(node => node.attributes.get("initial"))[0];
   const [firstCurrentNode, setFirstCurrentNode] = useState<NodeModel>(initialNode);
   const [secondCurrentNode, setSecondCurrentNode] = useState<NodeModel>(initialNode);
+
+  // last item is current node of second (since second starts)
+  const [visitedNodes, setVisitedNodes] = useState<NodeModel[]>([initialNode]);
   const [firstIsNext, setFirstIsNext] = useState<boolean>(false);
   // const [currentEdge, setCurrentEdge] = useState<EdgeModel | undefined>(undefined);
 
@@ -124,6 +129,8 @@ function CounterexampleDisplay() {
   function handleNextState(first: boolean) {
 
     let nextNodeTarget = first ? firstCurrentNode : secondCurrentNode;
+    if(!first)
+      setVisitedNodes(visitedNodes.concat(nextNodeTarget));
     // skip transitions with 0 delay (synchronizations)
     while(certificate.getOutgoingEdges(nextNodeTarget)[0].attributes.get("delay") == 0)
       nextNodeTarget = certificate.getOutgoingEdges(nextNodeTarget)[0].targets.at(1) as NodeModel;
@@ -135,68 +142,107 @@ function CounterexampleDisplay() {
     setFirstIsNext(!firstIsNext);
   }
 
-  function handlegoToInitialState() {
+  function handleGoToInitialState() {
     setFirstCurrentNode(initialNode);
     setSecondCurrentNode(initialNode);
+    setVisitedNodes([initialNode])
+  }
+
+  function handlePreviousStep() {
+    if(firstIsNext){
+      setFirstIsNext(false);
+      setSecondCurrentNode(visitedNodes[visitedNodes.length - 2]);
+      const newVisitedNodes = visitedNodes.filter((_, idx) => idx !== visitedNodes.length - 1);
+      setVisitedNodes(newVisitedNodes);
+    } else {
+      setFirstIsNext(true);
+      setFirstCurrentNode(visitedNodes[visitedNodes.length - 2]);
+    }
   }
 
   if(!firstSystem || !secondSystem)
     return (<h3 style={{ textAlign: 'center' }}>Loading</h3>)
 
-  const buttonSx = { display: 'flex', justifyContent: "center", alignItems: "center", overflowY: 'hidden', height: '100%', width: '100%'}
-
+  const firstCornerElement = 
+    <Grid item xs={12} sm={8} md={9} lg={9} sx={{ display: 'flex', alignItems: 'center', 
+      justifyContent: "center", overflow: 'auto', height: `${1.25/10 * contentHeight}px`, width: '20%', border: "1px solid grey" }}>
+        <IconButton
+          onMouseDown={() => swapTA()}
+          onKeyDown={(e) => executeOnKeyboardClick(e.key, () => swapTA())}
+          aria-label={t('tcheckerCounterexampleDisplay.button.swapTA')}
+        >
+          <SyncAltIcon/>
+        </IconButton>
+    </Grid>;
+  const secondCornerElement = 
+    <Grid item xs={12} sm={8} md={9} lg={9} sx={{ display: 'flex', flexDirection: 'row', alignItems: 'center', 
+      justifyContent: "center", overflow: 'auto', height: `${1.25/10 * contentHeight}px`, width: '20%', border: "1px solid grey" }}>
+        <IconButton
+          disabled={firstCurrentNode.id === initialNode.id && secondCurrentNode.id === initialNode.id}
+          onMouseDown={() => handlePreviousStep()}
+          onKeyDown={(e) => executeOnKeyboardClick(e.key, () => handlePreviousStep())}
+          aria-label={t('tcheckerCounterexampleDisplay.button.previousStep')}
+          size="small"
+        >
+          <UndoIcon/>
+        </IconButton>
+        &nbsp;
+        <Button
+          disabled={firstCurrentNode.id === initialNode.id && secondCurrentNode.id === initialNode.id}
+          onMouseDown={() => handleGoToInitialState()}
+          onKeyDown={(e) => executeOnKeyboardClick(e.key, () => handleGoToInitialState())}
+          variant="contained"
+        >
+          {t('tcheckerCounterexampleDisplay.button.reset')}
+        </Button>
+    </Grid>;
+  
   return (
     <>
-      <Box sx={{ display: 'flex',  height: `${7/8 * contentHeight}px`, overflow: 'hidden' }}>
-        <TAStateDisplay viewModel={firstViewModel} openedProcesses={firstOpenedProcesses} system={firstSystem} 
-         attributeNames={firstAttributes} contentHeight={contentHeight} currentNode={firstCurrentNode}/>
-        <TAStateDisplay viewModel={secondViewModel} openedProcesses={secondOpenedProcesses} system={secondSystem} 
-         attributeNames={secondAttributes} contentHeight={contentHeight} currentNode={secondCurrentNode}/>
+      <Box sx={{ display: 'flex',  height: `${9/10 * contentHeight}px`, overflow: 'hidden' }}>
+        <TAStateDisplay 
+          viewModel={firstViewModel} 
+          openedProcesses={firstOpenedProcesses} 
+          system={firstSystem} 
+          attributeNames={firstAttributes} 
+          contentHeight={contentHeight} 
+          currentNode={firstCurrentNode}
+          cornerElement={firstCornerElement}
+        />
+        <TAStateDisplay 
+          viewModel={secondViewModel} 
+          openedProcesses={secondOpenedProcesses} 
+          system={secondSystem} 
+          attributeNames={secondAttributes} 
+          contentHeight={contentHeight} 
+          currentNode={secondCurrentNode}
+          cornerElement={secondCornerElement}
+        />
       </Box>
-      <Box sx={{ display: 'flex', height: `${1/8 * contentHeight}px`, overflow: 'hidden', border: "1px solid grey" }}>
-        <Grid item xs={12} sm={8} md={9} lg={9} sx={buttonSx}>
+      <Box sx={{ display: 'flex', height: `${1/10 * contentHeight}px`, overflow: 'hidden', border: "1px solid grey" }}>
+        <Grid item xs={12} sm={8} md={9} lg={9} sx={{ display: 'flex', justifyContent: "center", alignItems: "center", overflowY: 'hidden', height: '100%', width: '40%'}}>
         </Grid>
-        <Grid item xs={12} sm={8} md={9} lg={9} sx={buttonSx}>
+        <Grid item xs={12} sm={8} md={9} lg={9} sx={{ display: 'flex', justifyContent: "center", alignItems: "center", overflowY: 'hidden', height: '100%', width: '10%'}}>
           <Button
-              disabled={firstCurrentNode.id === initialNode.id && secondCurrentNode.id === initialNode.id}
-              onMouseDown={() => handlegoToInitialState()}
-              onKeyDown={(e) => executeOnKeyboardClick(e.key, () => handlegoToInitialState())}
-              variant="contained"
+            disabled={!firstIsNext || certificate.getOutgoingEdges(firstCurrentNode).length === 0}
+            onMouseDown={() => handleNextState(true)}
+            onKeyDown={(e) => executeOnKeyboardClick(e.key, () => handleNextState(true))}
+            variant="contained"
           >
-              {t('tcheckerCounterexampleDisplay.button.reset')}
+            {t('tcheckerCounterexampleDisplay.button.nextStep')}
           </Button>
         </Grid>
-        <Grid item xs={12} sm={8} md={9} lg={9} sx={buttonSx}>
-          <Button
-              disabled={!firstIsNext || certificate.getOutgoingEdges(firstCurrentNode).length === 0}
-              onMouseDown={() => handleNextState(true)}
-              onKeyDown={(e) => executeOnKeyboardClick(e.key, () => handleNextState(true))}
-              variant="contained"
-          >
-              {t('tcheckerCounterexampleDisplay.button.nextStep')}
-          </Button>
+        <Grid item xs={12} sm={8} md={9} lg={9} sx={{ display: 'flex', justifyContent: "center", alignItems: "center", overflowY: 'hidden', height: '100%', width: '40%'}}>
         </Grid>
-        <Grid item xs={12} sm={8} md={9} lg={9} sx={buttonSx}>
+        <Grid item xs={12} sm={8} md={9} lg={9} sx={{ display: 'flex', justifyContent: "center", alignItems: "center", overflowY: 'hidden', height: '100%', width: '10%'}}>
           <Button
-              disabled={firstIsNext || certificate.getOutgoingEdges(secondCurrentNode).length === 0}
-              onMouseDown={() => handleNextState(false)}
-              onKeyDown={(e) => executeOnKeyboardClick(e.key, () => handleNextState(false))}
-              variant="contained"
+            disabled={firstIsNext || certificate.getOutgoingEdges(secondCurrentNode).length === 0}
+            onMouseDown={() => handleNextState(false)}
+            onKeyDown={(e) => executeOnKeyboardClick(e.key, () => handleNextState(false))}
+            variant="contained"
           >
-              {t('tcheckerCounterexampleDisplay.button.nextStep')}
+            {t('tcheckerCounterexampleDisplay.button.nextStep')}
           </Button>
-        </Grid>
-        <Grid item xs={12} sm={8} md={9} lg={9} sx={buttonSx}>
-          <Button
-              disabled={false}
-              onMouseDown={() => swapTA()}
-              onKeyDown={(e) => executeOnKeyboardClick(e.key, () => swapTA())}
-              variant="contained"
-          >
-              {t('tcheckerCounterexampleDisplay.button.swapTA')}
-          </Button>
-        </Grid>
-        <Grid item xs={12} sm={8} md={9} lg={9} sx={buttonSx}>
         </Grid>
       </Box>
     </>
--- frontend/src/pages/certificate/TAStateDisplay.tsx
+++ frontend/src/pages/certificate/TAStateDisplay.tsx
@@ -15,33 +15,38 @@ interface StateDisplayProps {
   attributeNames: Map<string, NodeAttributeKey>;
   contentHeight: number;
   currentNode: NodeModel;
+  cornerElement: JSX.Element;
 }
 
 const TAStateDisplay = (props: StateDisplayProps) => {
 
-  const { viewModel, openedProcesses, system, attributeNames, contentHeight, currentNode } = props;
+  const { viewModel, openedProcesses, system, attributeNames, contentHeight, currentNode, cornerElement } = props;
 
   const { t } = useTranslation();
 
-  return <Box sx={{ display: 'flex', flexDirection: 'column', height: `${7/8 * contentHeight}px`, width: '50%', overflow: 'hidden' }}>
-          <Grid item xs={12} sm={8} md={9} lg={9} sx={{ display: 'flex', flexDirection: 'row', alignItems: 'center', 
-            justifyContent: "center", overflow: 'auto', height: `${1/8 * contentHeight}px`, border: "1px solid grey" }}>
-            <h3 style={{ textAlign: 'center' }}>
-              {system.label}
-            </h3>
-            &nbsp;
-            <ProcessSelection viewModel={viewModel} openedProcesses={openedProcesses}/>
-            &nbsp;
-            <LayoutButton viewModel={viewModel} />
+  return <Box sx={{ display: 'flex', flexDirection: 'column', height: `${9/10 * contentHeight}px`, width: '50%', overflow: 'hidden' }}>
+          <Grid item xs={12} sm={8} md={9} lg={9} sx={{ display: 'flex', flexDirection: 'row', overflow: 'auto', height: `${1.25/10 * contentHeight}px`}}>
+            <Grid item xs={12} sm={8} md={9} lg={9} sx={{ display: 'flex', flexDirection: 'row', alignItems: 'center', 
+              justifyContent: "center", overflow: 'auto', height: `${1.25/10 * contentHeight}px`, width: '80%', border: "1px solid grey" }}>
+              <h3 style={{ textAlign: 'center' }}>
+                {system.label}
+              </h3>
+              &nbsp;
+              &nbsp;
+              <ProcessSelection viewModel={viewModel} openedProcesses={openedProcesses}/>
+              &nbsp;
+              <LayoutButton viewModel={viewModel} />
+            </Grid>
+            {cornerElement}
           </Grid>
-          <Box sx={{ display: 'flex', height: `${6/8 * contentHeight}px`, overflow: 'hidden' }}>
+          <Box sx={{ display: 'flex', height: `${7.75/10 * contentHeight}px`, overflow: 'hidden' }}>
             <Grid item xs={12} sm={8} md={9} lg={9} sx={{ overflow: 'hidden', width: '80%', border: "1px solid grey" }}>
               <AutomatonVisualization 
                 viewModel={viewModel} 
                 coloredLoc={currentNode.attributes.get(attributeNames.get("vloc"))[openedProcesses.automatonOptions.indexOf(openedProcesses.selectedOption)]} 
                 coloredSwitch='' />
             </Grid>
-            <Box sx={{ display: 'flex', flexDirection: 'column', height: `${6/8 * contentHeight}px`, width: '20%', overflow: 'hidden' }}>
+            <Box sx={{ display: 'flex', flexDirection: 'column', height: `${7.75/10 * contentHeight}px`, width: '20%', overflow: 'hidden' }}>
               <Grid item xs={12} sm={8} md={9} lg={9} sx={{ overflowY: 'auto', height: '50%', width: '100%', border: "1px solid grey" }}>
                 <h4 style={{ textAlign: 'center' }}> {t('manipulation.table.clockPlural')} </h4>
                 {Array.from((currentNode.attributes.get(attributeNames.get("clockval")) as Map<string, string>).keys()).map(clock => 
